54f0390f87e1679e85b8983288eeeb7e62a261bf
Viola Sorgato <egles.sorgato@medieinstitutet.se>
2023-05-16 17:00:08 +0200

    Added pages, links, components

--- app/router.js
+++ app/router.js
@@ -6,4 +6,11 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('about');
+  this.route('contact', { path: '/getting-in-touch' });
+});
+
+/*Here, we added the contact route, but explicitly specified 
+a path for the route. This allows us to keep the legacy URL, 
+but use the new, shorter name for the route, as well as the template filename. */
